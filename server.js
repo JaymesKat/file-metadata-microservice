37512faec17c53b5c5e2219fbeadac35c9682e9e
Glitch (succulent-squash-n8m0aqd0h) <none>
2020-02-05 06:13:16 +0000

    🎄💍 Checkpoint ./package.json:5655535/28 ./server.js:5655535/452

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+
 
 // require and use "multer"...
+const multer = require('multer');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+var upload = multer({ dest: '' })
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -16,6 +20,22 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload('single'), function(req, res){
+  
+  if(!req.file){
+    res.send('No file uploaded');
+  }
+  
+  const { file } = req;
+  
+  const resData = {
+    "name": file.originalname,
+    "type": file.mimetype,
+    "size": file.size,
+  }
+  res.json()
 });
 
 app.listen(process.env.PORT || 3000, function () {
